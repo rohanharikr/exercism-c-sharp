35f5f19e239ea81d625d3ff304fc7eaa690e1495
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 03:22:00 +0530

    Create Acronym.cs

---
+++ Solutions/Acronym.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        string[] words = phrase.Replace("_", "").Split(new char[]{'-', ' '});
+        StringBuilder acronym = new StringBuilder();
+        foreach(string word in words)
+        {
+            if(word.Length == 0)
+                continue;
+            acronym.Append(word[0]);
+        };
+        return acronym.ToString().ToUpper();
+    }
+}
