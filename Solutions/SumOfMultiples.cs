5adfb8aeaa9d3e7d1053339da54d2156a53ffa13
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 03:23:46 +0530

    Create SumOfMultiples.cs

---
+++ Solutions/SumOfMultiples.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        HashSet<int> m = new HashSet<int>();
+        foreach(int multiple in multiples)
+        {
+            for(int i=1;i<max; i++)
+            {
+                int s = multiple * i;
+                if(s >= max)
+                    break;
+                m.Add(s);
+            }
+        }
+        return m.Sum();
+    }
+}
