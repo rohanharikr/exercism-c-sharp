c48978740879b4a48f2c58f7f90874f8ec4717b9
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 03:21:36 +0530

    Create Isogram.cs

---
+++ Solutions/Isogram.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        string sanitizedWord = word.Replace(" ", "").Replace("-", "").ToLower();
+        List<char> checkedLetters = new List<char>();
+        foreach(char i in sanitizedWord.ToCharArray())
+        {
+            if(checkedLetters.Contains(i))
+                return false;
+            checkedLetters.Add(i);
+        };
+        return true;
+    }
+}
