31af7b80a5bca09cc46d6cdec523b2d3f6be8b82
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 03:19:20 +0530

    Create PhoneNumberAnalysis.cs

---
+++ Solutions/PhoneNumberAnalysis.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static class PhoneNumber
+{
+    public static (bool _IsNewYork, bool _IsFake, string _LocalNumber) Analyze(string phoneNumber)
+    {
+        string[] parsed = phoneNumber.Split("-");
+        bool _IsNewYork = parsed[0] == "212";
+        bool _IsFake = parsed[1] == "555";
+        string LocalNumber = parsed[2];
+        return ( _IsNewYork, _IsFake, LocalNumber );
+    }
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) => phoneNumberInfo.IsFake;
+}
