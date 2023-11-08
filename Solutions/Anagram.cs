12a5b1ce6f27bdd1ae3f12362610f5cfd2897c0c
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 03:25:36 +0530

    Create Anagram.cs

---
+++ Solutions/Anagram.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Anagram
+{
+    private string _baseWord;
+    public Anagram(string baseWord)
+    {
+        this._baseWord = baseWord.ToLower();
+    }
+
+    public string[] FindAnagrams(string[] potentialMatches)
+    {
+        List<string> matches = new List<string>();
+        foreach(string _word in potentialMatches)
+        {                
+            string word = _word.ToLower();
+            
+            if(word == _baseWord)
+                continue;
+            if(word.Length != _baseWord.Length)
+                continue;
+            
+            List<char> _baseWordArr = _baseWord.ToCharArray().ToList();
+                
+          foreach(char l in word.ToCharArray())
+          {
+              if(!_baseWordArr.Contains(l))
+                  break;
+              _baseWordArr.Remove(l);
+          };
+            if(_baseWordArr.Count == 0)
+                matches.Add(_word);
+        };
+        return matches.ToArray();
+    }
+}
