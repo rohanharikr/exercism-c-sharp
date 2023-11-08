80388daa8084db0cffb21e189411aa137a7c4849
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 03:12:05 +0530

    Create Luhn.cs

---
+++ Solutions/Luhn.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+
+public static class Luhn
+{
+    public static bool IsValid(string _number)
+    {
+        string number = _number.Replace(" ", "");
+        char[] numberArr = number.ToCharArray();
+        foreach(char n in numberArr)
+        {
+            if(!Char.IsNumber(n))
+                return false;
+        }
+        
+        if(number.Length <= 1)
+            return false;
+
+        int[] numList = (number.Select(digit => int.Parse(digit.ToString()))).ToArray();
+        bool skip = true;
+        for(int i=numList.Length - 1; i>=0; i--)
+        {
+            if(!skip)
+            {
+                int sum = (numList[i] * 2);
+                numList[i] = sum;
+                if(sum > 9)
+                {
+                    numList[i] = sum - 9;
+                }
+            }
+            skip = !skip;
+        };
+        return numList.Sum() % 10 == 0;
+    }
+}
