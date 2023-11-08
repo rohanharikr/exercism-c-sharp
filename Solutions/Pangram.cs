077496b917c988061e991d9e1adf4b3cd9b5f192
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 03:20:26 +0530

    Create Pangram.cs

---
+++ Solutions/Pangram.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        List<char> alphabets = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        List<char> inputMod = input.ToLower().ToCharArray().ToList();
+        for(int i=0; i<inputMod.Count; i++)
+        {
+            var letter = inputMod[i];
+            if(alphabets.Contains(letter))
+                alphabets.Remove(letter);
+        }
+        if(alphabets.Count > 0)
+            return false;
+        return true;
+    }
+}
