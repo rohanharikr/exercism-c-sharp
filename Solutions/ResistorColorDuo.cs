e5ce4b2a022e0b0987c416a5a1d801cf31adab39
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 03:23:11 +0530

    Create ResistorColorDuo.cs

---
+++ Solutions/ResistorColorDuo.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+public static class ResistorColorDuo
+{
+    private static Dictionary<string, int> _colorMap = new Dictionary<string, int>()
+    {
+        {"black", 0},
+        {"brown", 1},
+        {"red", 2},
+        {"orange", 3},
+        {"yellow", 4},
+        {"green", 5},
+        {"blue", 6},
+        {"violet", 7},
+        {"grey", 8},
+        {"white", 9}
+    };
+        
+    public static int Value(string[] colors)
+    {
+        StringBuilder display = new StringBuilder();
+        for(int i=0; i<2; i++)
+        {
+            int val = _colorMap[colors[i]];
+            display.Append(val);
+        };
+        return int.Parse(display.ToString());
+    }
+}
