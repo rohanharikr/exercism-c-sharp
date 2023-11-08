a020a724a0f0aad78008a68d8bf1c897d0b72d29
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 03:16:33 +0530

    Create BirdWatcher.cs

---
+++ BirdWatcher.cs
@@ -0,0 +1,47 @@
+using System;
+
+class BirdCount
+{
+    private int[] birdsPerDay;
+    private static int[] _birdsPerDayLastWeek = { 0, 2, 5, 3, 7, 8, 4 };
+
+    public BirdCount(int[] birdsPerDay)
+    {
+        this.birdsPerDay = birdsPerDay;
+    }
+
+    public static int[] LastWeek() => _birdsPerDayLastWeek;
+
+    public int Today() => birdsPerDay[birdsPerDay.Length - 1];
+
+    public void IncrementTodaysCount() => birdsPerDay[birdsPerDay.Length - 1] += 1;
+
+    public bool HasDayWithoutBirds()
+    {
+        foreach(int day in birdsPerDay) {
+            if(day == 0)
+                return true;
+        };
+        return false;
+    }
+
+    public int CountForFirstDays(int numberOfDays)
+    {
+        int sum = 0;
+        for(int i=0; i<numberOfDays; i++)
+        {
+            sum += birdsPerDay[i];
+        };
+        return sum;
+    }
+
+    public int BusyDays()
+    {
+        int busyDays = 0;
+        foreach(int day in birdsPerDay) {
+            if(day >= 5)
+                busyDays += 1;
+        };
+        return busyDays;
+    }
+}
