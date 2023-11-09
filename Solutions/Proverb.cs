4643325a66b203d892344e408274d0b5d43b5641
rohanharikr <rohanharikumar80@gmail.com>
2023-11-09 23:01:31 +0530

    Create Proverb.cs

---
+++ Solutions/Proverb.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        if(subjects.Length == 0)
+            return new string[0];
+        List<string> proverbs = new List<string>();
+        for(int i=0; i < subjects.Length - 1; i++)
+        {
+            proverbs.Add($"For want of a {subjects[i]} the {subjects[i + 1]} was lost.");
+        }
+        proverbs.Add($"And all for the want of a {subjects[0]}.");
+        
+        return proverbs.ToArray();
+    }
+}
